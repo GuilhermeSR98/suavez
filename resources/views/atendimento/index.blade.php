7a9b5333b26093a1d5eeb35506acbe597f82ba65
Guilherme Reis <guilherme.reis@coretecnologia.net.br>
2023-12-25 19:01:31 -0300

    Ajustes no front

--- resources/views/atendimento/index.blade.php
+++ resources/views/atendimento/index.blade.php
@@ -1,12 +1,12 @@
 <x-wrapper title="Sua Vez">
-    <div class="general-content d-flex flex-column align-items-center vh-100 mt-5">
+    <div class="general-content d-flex flex-column align-items-center vh-80 mt-5">
         <div class="header d-flex flex-column align-items-center mb-5">
             <div class="border border-dark border-3 rounded-circle p-4">
                 <img src="{{ URL('storage/assets/images/logo.svg') }}" alt="Logo cabeçalho Adoorei">
             </div>
             <h1 class="mt-4">Sua vez!</h1>
         </div>
-        <div class="registro-atendimento p-4 w-50 rounded d-flex flex-column align-items-center border border-light" style="box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); transition: box-shadow 0.3s ease-in-out;">
+        <div class="registro-atendimento p-4 w-75 rounded d-flex flex-column align-items-center border border-light" style="box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); transition: box-shadow 0.3s ease-in-out;">
             <div class="proximo-atendente">
                 <h2>Próximo a pegar o chat: <strong>{{$atendentes[$controleAtendente->indice_proximo]->nome}}</strong></h2>
             </div>
@@ -19,7 +19,7 @@
         </div>
         <div class="mt-3">
             <a class="btn btn-primary m-3" href="{{ route('atendente.peguei') }}">Peguei</a>
-            <a class="btn btn-secondary m-3" href="{{ route('atendente.index') }}">Atendentes</a>
+            <a class="btn btn-secondary m-3" href="{{ route('atendente.index') }}">Editar Atendentes</a>
         </div>
     </div>
 </x-wrapper>
